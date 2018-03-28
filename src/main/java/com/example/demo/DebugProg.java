3b1a63e34513d6a9690e31c0f8f0fffa6e0fd7dd
Daniela Sainz <dasainz18@gmail.com>
2018-03-28 16:18:46 -0400

    changed data type

--- src/main/java/com/example/demo/DebugProg.java
+++ src/main/java/com/example/demo/DebugProg.java
@@ -3,6 +3,7 @@ package com.example.demo;
 public class DebugProg {
     public static void main(String args []) {
 
+        // changed int to float
         float x = 3.145F;
         float y = 3.64F;
 
