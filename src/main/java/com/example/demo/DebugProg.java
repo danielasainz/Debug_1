1ebbd2851bf1a4983e1a7e504ad68407df1dc875
Daniela Sainz <dasainz18@gmail.com>
2018-03-28 15:16:12 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/DebugProg.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class DebugProg {
+    public static void main(String args []) {
+
+        int x = (int) 3.145;
+        int y = (int) 3.64;
+
+        System.out.println("pi is approximately " + x);
+        System.out.println("My GPA was " + y);
+    }
+}
